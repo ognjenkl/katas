4593da66583cb2a3deea4cc468628c7906965309
ognjenklincov <ognjenkl@gmail.com>
2023-04-09 21:13:24 +0200

    Set Observed test init

---
+++ katas/src/main/java/tdd/observed/pin/ObservedPin.java
@@ -0,0 +1,10 @@
+package tdd.observed.pin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObservedPin {
+    public static List<String> getPINs(String observed) {
+        return new ArrayList<>();
+    }
+}
