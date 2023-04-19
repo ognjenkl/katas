57d361c4a884f026b4e23ab9a0b4dee9266e3f0d
ognjenklincov <ognjenkl@gmail.com>
2023-04-19 23:25:53 +0200

    First pass

--- katas/src/main/java/tdd/observed/pin/ObservedPin.java
+++ katas/src/main/java/tdd/observed/pin/ObservedPin.java
@@ -5,25 +5,21 @@ import java.util.List;
 
 public class ObservedPin {
     public static List<String> getPINs(String observed) {
-        return getPINList(observed);
+        List<String> list = new ArrayList<>();
+        getPINList(observed, "", list);
+        return list;
     }
 
-    private static List<String> getPINList(String observed) {
-        if (observed.length() == 1) {
-            return getPINLengthOne(observed);
-        } else if (observed.length() == 2) {
-            String[] ob = observed.split("");
-            List<String> list = new ArrayList<>();
-            for (String first : getPINLengthOne(ob[0])) {
-                for (String second : getPINLengthOne(ob[1])) {
-                    list.add(first + second);
-                }
-            }
-            return list;
+    private static void getPINList(String observed, String prefix, List<String> list) {
+        if (observed.length() == 0) {
+            list.add(prefix);
         } else {
-            String[] pins = observed.split("");
-            System.out.println(List.of(pins));
-            return new ArrayList<>();
+            String first = observed.substring(0, 1);
+            String rest = observed.substring(1);
+
+            for (String firstPIN : getPINLengthOne(first)) {
+                getPINList(rest, prefix + firstPIN, list);
+            }
         }
     }
 
@@ -34,21 +30,21 @@ public class ObservedPin {
             case "2":
                 return List.of("1", "2", "3", "5");
             case "3":
-                return List.of("2", "6");
+                return List.of("2", "3", "6");
             case "4":
                 return List.of("1", "4", "5", "7");
             case "5":
                 return List.of("2", "4", "5", "6", "8");
             case "6":
-                return List.of("3", "5", "9");
+                return List.of("3", "5", "6", "9");
             case "7":
-                return List.of("4", "8");
+                return List.of("4", "7", "8");
             case "8":
-                return List.of("5", "7", "9", "0");
+                return List.of("5", "7", "8", "9", "0");
             case "9":
-                return List.of("6", "8");
+                return List.of("6", "8", "9");
             case "0":
-                return List.of("8");
+                return List.of("8", "0");
         }
         throw new RuntimeException("Not a good observed PIN is entered");
     }
