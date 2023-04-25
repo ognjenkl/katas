0503c4b7955d85de4c3078ab0c89202f4c961fc1
ognjenklincov <ognjenkl@gmail.com>
2023-04-26 00:01:39 +0200

    Refactoring and new test

--- katas/src/main/java/tdd/human/readable/duration/format/TimeFormatter.java
+++ katas/src/main/java/tdd/human/readable/duration/format/TimeFormatter.java
@@ -1,5 +1,8 @@
 package tdd.human.readable.duration.format;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TimeFormatter {
 
     public static final String SECOND = "second";
@@ -9,42 +12,129 @@ public class TimeFormatter {
     public static final String AND = "and";
     public static final String DELIMITER = SPACE + AND + SPACE;
     public static final String MINUTES = "minutes";
+    public static final String EMPTY_STRING = "";
+    public static final String NOW = "now";
+    public static final String YEARS = "years";
+    public static final String YEAR = "year";
+    public static final String DAYS = "days";
+    public static final String DAY = "day";
+    public static final String HOURS = "hours";
+    public static final String HOUR = "hour";
+    private static String retVal;
 
     public static String formatDuration(int seconds) {
-//        if (seconds < 60) {
-//            return processSeconds(seconds);
-//        } else {
         return process(seconds);
-//        }
     }
 
     private static String process(int seconds) {
-        String retVal = "";
 
-        int secs = seconds % 60;
-        int minutes = seconds / 60;
-
-        if (minutes > 0) {
-            retVal = formatMinutes(minutes);
-            if (secs > 0) {
-                retVal += DELIMITER + formatSeconds(secs);
-            }
-        } else if (secs > 0) {
-            retVal += formatSeconds(secs);
+        if (seconds == 0) {
+            return NOW;
         }
 
+        retVal = EMPTY_STRING;
+
+        int years = seconds / (60 * 60 * 24 * 365);
+        int yearsMod = seconds % (60 * 60 * 24 * 365);
+        int days = yearsMod / (60 * 60 * 24);
+        int daysMod = yearsMod % (60 * 60 * 24);
+        int hours = daysMod / (60 * 60);
+        int hoursMod = daysMod % (60 * 60);
+        int minutes = hoursMod / 60;
+        int minutesMod = hoursMod % 60;
+        int secs = minutesMod % 60;
+
+        String yearsFormatted = formatUnits(years, YEARS, YEAR);
+        processUnits(yearsFormatted, false);
+
+        String daysFormatted = formatUnits(days, DAYS, DAY);
+        processUnits(daysFormatted, yearsNeedDelimiter(yearsFormatted));
+
+        String hoursFormatted = formatUnits(hours, HOURS, HOUR);
+        processUnits(hoursFormatted, hoursNeedDelimiter(yearsFormatted, daysFormatted));
+
+        String minutesFormatted = formatUnits(minutes, MINUTES, MINUTE);
+        processUnits(minutesFormatted, minutesNeedDelimiter(
+                yearsFormatted, daysFormatted, hoursFormatted));
+
+        String secondsFormatted = formatUnits(secs, SECONDS, SECOND);
+        processUnits(secondsFormatted, secondsNeedDelimiter(
+                yearsFormatted, daysFormatted, hoursFormatted, minutesFormatted));
+
+        formantResult();
         return retVal;
     }
 
-    private static String formatMinutes(int minutes) {
-        String retVal;
-        String postfix = minutes == 1 ? MINUTE : MINUTES;
-        retVal = minutes + SPACE + postfix;
-        return retVal;
+    private static boolean yearsNeedDelimiter(String yearsFormatted) {
+        return !yearsFormatted.isEmpty();
     }
 
-    private static String formatSeconds(int seconds) {
-        String postfix = seconds == 1 ? SECOND : SECONDS;
-        return seconds + SPACE + postfix;
+    private static void formantResult() {
+        int countAnd = countAll(retVal, AND);
+        if (countAnd > 1) {
+            retVal = replaceByCount(
+                    retVal, SPACE + AND, ",", countAnd - 1);
+        }
+    }
+
+    private static void processUnits(String unitsFormatted, boolean isDelimiterNeeded) {
+        if (!unitsFormatted.isEmpty()) {
+            if (isDelimiterNeeded) {
+                retVal += DELIMITER;
+            }
+            retVal += unitsFormatted;
+        }
+    }
+
+    private static String formatUnits(int unit, String unitPlural, String unitSingular) {
+        String daysFormatted = EMPTY_STRING;
+        if (unit > 1) {
+            daysFormatted += unit + SPACE + unitPlural;
+        } else if (unit == 1) {
+            daysFormatted += unit + SPACE + unitSingular;
+        }
+        return daysFormatted;
+    }
+
+    private static boolean hoursNeedDelimiter(
+            String yearsFormatted, String daysFormatted) {
+        return !yearsFormatted.isEmpty() || !daysFormatted.isEmpty();
+    }
+
+    private static boolean minutesNeedDelimiter(
+            String yearsFormatted, String daysFormatted, String hoursFormatted) {
+        return !yearsFormatted.isEmpty()
+                || !daysFormatted.isEmpty()
+                || !hoursFormatted.isEmpty();
+    }
+
+    private static boolean secondsNeedDelimiter(
+            String yearsFormatted, String daysFormatted,
+            String hoursFormatted, String minutesFormatted) {
+        return !yearsFormatted.isEmpty()
+                || !daysFormatted.isEmpty()
+                || !hoursFormatted.isEmpty()
+                || !minutesFormatted.isEmpty();
+    }
+
+    private static String replaceByCount(
+            String text, String subString, String replaceWith, int count) {
+        Pattern pattern = Pattern.compile(subString);
+        Matcher matcher = pattern.matcher(text);
+        while (count > 0 && matcher.find()) {
+            text = text.replaceFirst(subString, replaceWith);
+            count--;
+        }
+        return text;
+    }
+
+    private static int countAll(String text, String subString) {
+        int count = 0;
+        Pattern pattern = Pattern.compile(subString);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
     }
 }
--- katas/src/test/java/tdd/human/readable/duration/format/TimeFormatterTest.java
+++ katas/src/test/java/tdd/human/readable/duration/format/TimeFormatterTest.java
@@ -60,6 +60,12 @@ unit of time.
 public class TimeFormatterTest {
 
     @Test
+    void shouldReturnNow() {
+        String result = TimeFormatter.formatDuration(0);
+        Assertions.assertEquals("now", result);
+    }
+
+    @Test
     void shouldReturn1Second() {
         String result = TimeFormatter.formatDuration(1);
         Assertions.assertEquals("1 second", result);
@@ -137,9 +143,349 @@ public class TimeFormatterTest {
         Assertions.assertEquals("59 minutes and 59 seconds", result);
     }
 
-//    @Test
-//    void shouldReturn1Hour() {
-//        String result = TimeFormatter.formatDuration(3600);
-//        Assertions.assertEquals("1 hour", result);
-//    }
+    @Test
+    void shouldReturn2Years() {
+        String result = TimeFormatter.formatDuration(63072000);
+        Assertions.assertEquals("2 years", result);
+    }
+
+    @Test
+    void shouldReturn1Years() {
+        String result = TimeFormatter.formatDuration(31536000);
+        Assertions.assertEquals("1 year", result);
+    }
+
+    @Test
+    void shouldReturn2Days() {
+        String result = TimeFormatter.formatDuration(172800);
+        Assertions.assertEquals("2 days", result);
+    }
+
+    @Test
+    void shouldReturn1Day() {
+        String result = TimeFormatter.formatDuration(86400);
+        Assertions.assertEquals("1 day", result);
+    }
+
+    @Test
+    void shouldReturn2Hours() {
+        String result = TimeFormatter.formatDuration(7200);
+        Assertions.assertEquals("2 hours", result);
+    }
+
+    @Test
+    void shouldReturn1Hour() {
+        String result = TimeFormatter.formatDuration(3600);
+        Assertions.assertEquals("1 hour", result);
+    }
+
+    @Test
+    void shouldReturn1HourAnd1Second() {
+        String result = TimeFormatter.formatDuration(3601);
+        Assertions.assertEquals("1 hour and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1HourAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(3602);
+        Assertions.assertEquals("1 hour and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1HourAnd59Seconds() {
+        String result = TimeFormatter.formatDuration(3659);
+        Assertions.assertEquals("1 hour and 59 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1HourAnd1Minute() {
+        String result = TimeFormatter.formatDuration(3660);
+        Assertions.assertEquals("1 hour and 1 minute", result);
+    }
+
+    @Test
+    void shouldReturn1Hour1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(3661);
+        Assertions.assertEquals("1 hour, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Hour1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(3662);
+        Assertions.assertEquals("1 hour, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1HourMinuteAnd59Seconds() {
+        String result = TimeFormatter.formatDuration(3719);
+        Assertions.assertEquals("1 hour, 1 minute and 59 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1HourAnd2Minutes() {
+        String result = TimeFormatter.formatDuration(3720);
+        Assertions.assertEquals("1 hour and 2 minutes", result);
+    }
+
+    @Test
+    void shouldReturn1Hour2MinutesAnd1Second() {
+        String result = TimeFormatter.formatDuration(3721);
+        Assertions.assertEquals("1 hour, 2 minutes and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Hour2MinutesAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(3722);
+        Assertions.assertEquals("1 hour, 2 minutes and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Hour59Minutes59Seconds() {
+        String result = TimeFormatter.formatDuration(7199);
+        Assertions.assertEquals("1 hour, 59 minutes and 59 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2HoursAnd1Second() {
+        String result = TimeFormatter.formatDuration(7201);
+        Assertions.assertEquals("2 hours and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2HoursAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(7202);
+        Assertions.assertEquals("2 hours and 2 seconds", result);
+    }
+
+    @Test
+    void shouldREturn2HoursAnd59Seconds() {
+        String result = TimeFormatter.formatDuration(7259);
+        Assertions.assertEquals("2 hours and 59 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2Hours1Minute() {
+        String result = TimeFormatter.formatDuration(7260);
+        Assertions.assertEquals("2 hours and 1 minute", result);
+    }
+
+    @Test
+    void shouldReturn2Hours1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(7261);
+        Assertions.assertEquals("2 hours, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2Hours1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(7262);
+        Assertions.assertEquals("2 hours, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2Hours2MinutesAnd1Second() {
+        String result = TimeFormatter.formatDuration(7321);
+        Assertions.assertEquals("2 hours, 2 minutes and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2Hours2MinutesAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(7322);
+        Assertions.assertEquals("2 hours, 2 minutes and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn23Hours59Minutes59Seconds() {
+        String result = TimeFormatter.formatDuration(86399);
+        Assertions.assertEquals("23 hours, 59 minutes and 59 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1DayAnd1Second() {
+        String result = TimeFormatter.formatDuration(86401);
+        Assertions.assertEquals("1 day and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1DayAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(86402);
+        Assertions.assertEquals("1 day and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Day1Minute() {
+        String result = TimeFormatter.formatDuration(86460);
+        Assertions.assertEquals("1 day and 1 minute", result);
+    }
+
+    @Test
+    void shouldReturn1Day1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(86461);
+        Assertions.assertEquals("1 day, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Day1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(86462);
+        Assertions.assertEquals("1 day, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Day1Hour() {
+        String result = TimeFormatter.formatDuration(90000);
+        Assertions.assertEquals("1 day and 1 hour", result);
+    }
+
+    @Test
+    void shouldReturn1Day1HourAnd1Second() {
+        String result = TimeFormatter.formatDuration(90001);
+        Assertions.assertEquals("1 day, 1 hour and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Day1Hour1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(90061);
+        Assertions.assertEquals("1 day, 1 hour, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Day1Hour1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(90062);
+        Assertions.assertEquals("1 day, 1 hour, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Day1Hour2MinutesAnd1Second() {
+        String result = TimeFormatter.formatDuration(90121);
+        Assertions.assertEquals("1 day, 1 hour, 2 minutes and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Day1Hour2MinutesAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(90122);
+        Assertions.assertEquals("1 day, 1 hour, 2 minutes and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Day2Hours1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(
+                1 * 24 * 60 * 60 + +2 * 60 * 60 + 1 * 60 + 1);
+        Assertions.assertEquals("1 day, 2 hours, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Day2Hours1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(
+                1 * 24 * 60 * 60 + +2 * 60 * 60 + 1 * 60 + 2);
+        Assertions.assertEquals("1 day, 2 hours, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2Days1Hour1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +1 * 60 * 60 + 1 * 60 + 1);
+        Assertions.assertEquals("2 days, 1 hour, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2Days1Hour1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +1 * 60 * 60 + 1 * 60 + 2);
+        Assertions.assertEquals("2 days, 1 hour, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2Days1Hour2MinutesAnd1Second() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +1 * 60 * 60 + 2 * 60 + 1);
+        Assertions.assertEquals("2 days, 1 hour, 2 minutes and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2Days1Hour2MinutesAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +1 * 60 * 60 + 2 * 60 + 2);
+        Assertions.assertEquals("2 days, 1 hour, 2 minutes and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2Days2Hours1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +2 * 60 * 60 + 1 * 60 + 1);
+        Assertions.assertEquals("2 days, 2 hours, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2Days2Hours1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +2 * 60 * 60 + 1 * 60 + 2);
+        Assertions.assertEquals("2 days, 2 hours, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn2Days2Hours2MinutesAnd1Second() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +2 * 60 * 60 + 2 * 60 + 1);
+        Assertions.assertEquals("2 days, 2 hours, 2 minutes and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn2Days2Hours2MinutesAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(
+                2 * 24 * 60 * 60 + +2 * 60 * 60 + 2 * 60 + 2);
+        Assertions.assertEquals("2 days, 2 hours, 2 minutes and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Day1HourAnd1Minute() {
+        String result = TimeFormatter.formatDuration(90060);
+        Assertions.assertEquals("1 day, 1 hour and 1 minute", result);
+    }
+
+    @Test
+    void shouldReturn1Year() {
+        String result = TimeFormatter.formatDuration(31536000);
+        Assertions.assertEquals("1 year", result);
+    }
+
+    @Test
+    void shouldReturn1Year1Second() {
+        String result = TimeFormatter.formatDuration(31536001);
+        Assertions.assertEquals("1 year and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Year1Minute() {
+        String result = TimeFormatter.formatDuration(31536060);
+        Assertions.assertEquals("1 year and 1 minute", result);
+    }
+
+    @Test
+    void shouldReturn1Year1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(31536061);
+        Assertions.assertEquals("1 year, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Year1Hour1MinuteAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(31539662);
+        Assertions.assertEquals("1 year, 1 hour, 1 minute and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Year1Day1Hour1MinuteAnd1Second() {
+        String result = TimeFormatter.formatDuration(31626061);
+        Assertions.assertEquals("1 year, 1 day, 1 hour, 1 minute and 1 second", result);
+    }
+
+    @Test
+    void shouldReturn1Year1Day1HourAnd2Seconds() {
+        String result = TimeFormatter.formatDuration(31626002);
+        Assertions.assertEquals("1 year, 1 day, 1 hour and 2 seconds", result);
+    }
+
+    @Test
+    void shouldReturn1Year1Day1HourAnd1Minute() {
+        String result = TimeFormatter.formatDuration(31626060);
+        Assertions.assertEquals("1 year, 1 day, 1 hour and 1 minute", result);
+    }
 }
